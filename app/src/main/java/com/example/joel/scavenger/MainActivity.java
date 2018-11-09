786445cb4e8f2555ae1fda6e8938bfaa529ca46a
JoelAkwam <jakwam@gmail.com>
2018-11-09 23:15:56 +0300

    Bind search Button and EditText with ButterKnife

--- app/src/main/java/com/example/joel/scavenger/MainActivity.java
+++ app/src/main/java/com/example/joel/scavenger/MainActivity.java
@@ -2,14 +2,22 @@ package com.example.joel.scavenger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    @BindView(R.id.searchField) EditText searchField;
+    @BindView(R.id.searchButton) Button searchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
     }
 }
