1ee20a01255da5a5a587344cbd3f05eb9cc2e8f2
JoelAkwam <jakwam@gmail.com>
2018-11-09 22:24:05 +0300

    Bind TextView in SplashActivity with ButterKnife

--- app/src/main/java/com/example/joel/scavenger/SplasActivity.java
+++ app/src/main/java/com/example/joel/scavenger/SplasActivity.java
@@ -4,8 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.TextView;
+
+import butterknife.BindView;
 
 public class SplasActivity extends Activity {
+
+    @BindView(R.id.appTitle) TextView appTitle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
