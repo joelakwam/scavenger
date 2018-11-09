8ef306bdb14a757d07aa32e04242976882d7fe42
JoelAkwam <jakwam@gmail.com>
2018-11-09 23:24:13 +0300

    Attach onClickListener to the searchButton

--- app/src/main/java/com/example/joel/scavenger/MainActivity.java
+++ app/src/main/java/com/example/joel/scavenger/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.joel.scavenger;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,5 +22,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String location = searchField.getText().toString();
+
+//                if(location.isEmpty()){
+//                    Toast.makeText(MainActivity.this, "Please enter location", Toast.LENGTH_LONG).show();
+//                }else{
+//                    Intent intent = new Intent(MainActivity.this, EventsActivity.class);
+//                    intent.putExtra("location", location);
+//                    startActivity(intent);
+//                }
+            }
+        });
     }
 }
