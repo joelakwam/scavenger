f93d1b7109da83ce2efa5b715683238e4b382b75
JoelAkwam <jakwam@gmail.com>
2018-11-09 22:08:41 +0300

    Implement splash in SplashActivity class

--- app/src/main/java/com/example/joel/scavenger/SplasActivity.java
+++ app/src/main/java/com/example/joel/scavenger/SplasActivity.java
@@ -1,4 +1,25 @@
 package com.example.joel.scavenger;
 
-public class SplasActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplasActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_file);
+
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplasActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
 }
