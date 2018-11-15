ef285b8f3e8b0e58c39b45db21c712012308fd37
JoelAkwam <jakwam@gmail.com>
2018-11-15 12:14:48 +0300

    Create EventService java class

--- app/src/main/java/com/example/joel/scavenger/Constants.java
+++ app/src/main/java/com/example/joel/scavenger/Constants.java
@@ -2,4 +2,7 @@ package com.example.joel.scavenger;
 
 public class Constants {
     public static final String ACCESS_TOKEN = BuildConfig.ACCESS_TOKEN;
+    public static final String BASE_URL = "https://www.eventbriteapi.com/v3/events/search/";
+    public static final String QUERY_PARAMETER = "location.address";
+
 }
