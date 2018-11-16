615936ae2acaf7e91489204b79615dd921494e10
JoelAkwam <jakwam@gmail.com>
2018-11-16 21:12:14 +0300

    Create a constructor inside the Event model class

--- app/src/main/java/com/example/joel/scavenger/models/Event.java
+++ app/src/main/java/com/example/joel/scavenger/models/Event.java
@@ -8,4 +8,14 @@ public class Event{
     private String endTime;
     private String status;
     private String currency;
+
+    public Event(String name, String description, String url, String startTime, String endTime, String status, String currency){
+        this.name = name;
+        this.description = description;
+        this.url = url;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.status = status;
+        this.currency = currency;
+    }
 }
