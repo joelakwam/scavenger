e97b105ecbb7c0c96b8e010e71728e0c1089613f
JoelAkwam <jakwam@gmail.com>
2018-11-16 21:08:33 +0300

    Add required member variables inside the Event model class

--- app/src/main/java/com/example/joel/scavenger/models/Event.java
+++ app/src/main/java/com/example/joel/scavenger/models/Event.java
@@ -1,4 +1,11 @@
 package com.example.joel.scavenger.models;
 
-public class Event {
+public class Event{
+    private String name;
+    private String description;
+    private String url;
+    private String startTime;
+    private String endTime;
+    private String status;
+    private String currency;
 }
