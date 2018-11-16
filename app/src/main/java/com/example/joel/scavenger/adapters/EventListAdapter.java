44f922f99ea8e810c89e72da39bd3dfcdc254f0e
JoelAkwam <jakwam@gmail.com>
2018-11-17 01:08:57 +0300

    Create EventViewHolder nested class inside the EventListAdapter class that extends RecyclerViewHolder class

--- app/src/main/java/com/example/joel/scavenger/adapters/EventListAdapter.java
+++ app/src/main/java/com/example/joel/scavenger/adapters/EventListAdapter.java
@@ -1,6 +1,48 @@
 package com.example.joel.scavenger.adapters;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.joel.scavenger.R;
+import com.example.joel.scavenger.models.Event;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.EventViewHolder> {
+    private ArrayList<Event> events = new ArrayList<>();
+    private Context context;
+
+    public EventListAdapter(Context context, ArrayList<Event> events){
+        this.context = context;
+        this.events = events;
+    }
+
+    @Override
+    public EventViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    public class EventViewHolder extends RecyclerView.ViewHolder{
+        @BindView(R.id.eventImageView) ImageView eventImageView;
+        @BindView(R.id.eventNameTextView) TextView eventNameTextView;
+        private Context context;
+
+        public EventViewHolder(View itemView){
+            super(itemView);
+            ButterKnife.bind(this);
+            context = itemView.getContext();
+        }
+
+        public void bindEvent(Event event){
+            eventNameTextView.setText(event.getName());
+        }
+    }
 }
