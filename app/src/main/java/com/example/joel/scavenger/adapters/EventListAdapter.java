c06736407183e70be026a861510e10a3e184edc4
JoelAkwam <jakwam@gmail.com>
2018-11-17 00:53:42 +0300

    Create an EventListAdapter that extends the RecyclerView class

--- app/src/main/java/com/example/joel/scavenger/adapters/EventListAdapter.java
+++ app/src/main/java/com/example/joel/scavenger/adapters/EventListAdapter.java
@@ -1,4 +1,6 @@
-//package com.example.joel.scavenger.adapters;
-//
-//public class EventListAdapter {
-//}
+package com.example.joel.scavenger.adapters;
+
+import android.support.v7.widget.RecyclerView;
+
+public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.EventViewHolder> {
+}
