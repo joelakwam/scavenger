677fa9c7514ae6a362929102d484336fb264bd1a
JoelAkwam <jakwam@gmail.com>
2018-11-16 22:05:15 +0300

    Create a processResult method that returns an array list of events

--- app/src/main/java/com/example/joel/scavenger/models/Event.java
+++ app/src/main/java/com/example/joel/scavenger/models/Event.java
@@ -6,15 +6,17 @@ public class Event{
     private String url;
     private String startTime;
     private String endTime;
+    private String timeZone;
     private String status;
     private String currency;
 
-    public Event(String name, String description, String url, String startTime, String endTime, String status, String currency){
+    public Event(String name, String description, String url, String startTime, String endTime, String timeZone,String status, String currency){
         this.name = name;
         this.description = description;
         this.url = url;
         this.startTime = startTime;
         this.endTime = endTime;
+        this.timeZone = timeZone;
         this.status = status;
         this.currency = currency;
     }
@@ -37,6 +39,10 @@ public class Event{
 
     public String getEndTime(){
         return endTime;
+    }
+
+    public String getTimeZone(){
+        return timeZone;
     }
 
     public String getStatus(){
--- app/src/main/java/com/example/joel/scavenger/services/EventService.java
+++ app/src/main/java/com/example/joel/scavenger/services/EventService.java
@@ -2,12 +2,21 @@ package com.example.joel.scavenger.services;
 
 
 import com.example.joel.scavenger.Constants;
+import com.example.joel.scavenger.models.Event;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 
 public class EventService{
 
@@ -28,5 +37,38 @@ public class EventService{
         //Creating a callback received from the API
         Call call = client.newCall(request);
         call.enqueue(callback);
+    }
+
+    //Method that will return an array list of Event objects
+    public static ArrayList<Event> processResults(Response response){
+
+        //ArrayList to hold a list of the required information.
+        ArrayList<Event> eventArrayList = new ArrayList<>();
+
+        try{
+            String jsonData = response.body().string();
+            JSONObject eventsbriteJSON = new JSONObject(jsonData);
+            JSONArray eventsJSON = eventsbriteJSON.getJSONArray("events");
+
+            for(int i = 0; i < eventsJSON.length(); i++){
+                JSONObject event = eventsJSON.getJSONObject(i);
+                String name = event.getJSONObject("name").getString("text");
+                String description = event.getJSONObject("description").getString("text");
+                String url = event.getString("url");
+                String startTime = event.getJSONObject("start").getString("local");
+                String endTime = event.getJSONObject("end").getString("local");
+                String timeZone = event.getJSONObject("start").getString("timezone");
+                String status = event.getString("status");
+                String currency = event.getString("currency");
+
+                Event events = new Event(name, description, url, startTime, endTime, timeZone, status, currency);
+                eventArrayList.add(events);
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }catch(JSONException e){
+            e.printStackTrace();
+        }
+        return eventArrayList;
     }
 }
