48b44e4e4bb99e93bbadc0c7b9ce78f39a93b121
JoelAkwam <jakwam@gmail.com>
2018-11-17 10:46:19 +0300

    Implement RecyclerView into project

--- app/src/main/java/com/example/joel/scavenger/adapters/EventListAdapter.java
+++ app/src/main/java/com/example/joel/scavenger/adapters/EventListAdapter.java
@@ -3,6 +3,7 @@ package com.example.joel.scavenger.adapters;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -26,23 +27,44 @@ public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.Even
     }
 
     @Override
-    public EventViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        return null;
+    public EventListAdapter.EventViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.event_list_item, viewGroup, false);
+
+        EventViewHolder viewHolder = new EventViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(EventListAdapter.EventViewHolder holder, int i){
+        holder.bindEvent(events.get(i));
+    }
+
+    @Override
+    public int getItemCount() {
+        return events.size();
     }
 
     public class EventViewHolder extends RecyclerView.ViewHolder{
         @BindView(R.id.eventImageView) ImageView eventImageView;
         @BindView(R.id.eventNameTextView) TextView eventNameTextView;
+        @BindView(R.id.statusTextView) TextView statusTextView;
+        @BindView(R.id.startTimeTextView) TextView startTimeTextView;
+        @BindView(R.id.endTimeTextView) TextView endTimeTextView;
+        @BindView(R.id.timeZoneTextView) TextView timeZoneTextView;
         private Context context;
 
         public EventViewHolder(View itemView){
             super(itemView);
-            ButterKnife.bind(this);
+            ButterKnife.bind(this, itemView);
             context = itemView.getContext();
         }
 
         public void bindEvent(Event event){
             eventNameTextView.setText(event.getName());
+            statusTextView.setText(event.getStatus());
+            startTimeTextView.setText("Start: " + event.getStartTime());
+            endTimeTextView.setText("End: " + event.getEndTime());
+            timeZoneTextView.setText("Timezone: " + event.getTimeZone());
         }
     }
 }
