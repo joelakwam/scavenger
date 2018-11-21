b829833b283ea06102007a4b2f2e08556e1b7968
JoelAkwam <jakwam@gmail.com>
2018-11-21 19:57:01 +0300

    Change font

--- app/src/main/java/com/example/joel/scavenger/ui/LoginActivity.java
+++ app/src/main/java/com/example/joel/scavenger/ui/LoginActivity.java
@@ -1,11 +1,14 @@
 package com.example.joel.scavenger.ui;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.joel.scavenger.R;
 
@@ -15,6 +18,9 @@ import butterknife.ButterKnife;
 public class LoginActivity extends AppCompatActivity{
     @BindView(R.id.toRegisterLink) TextView toRegisterLink;
     @BindView(R.id.loginButton) Button loginButton;
+    @BindView(R.id.emailField) EditText inputEmail;
+    @BindView(R.id.password) EditText inputPassword;
+    @BindView(R.id.loginHeading) TextView loginHeading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +29,23 @@ public class LoginActivity extends AppCompatActivity{
 
         ButterKnife.bind(this);
 
+        Typeface varelaRound = Typeface.createFromAsset(getAssets(), "fonts/VarelaRound-Regular.ttf");
+        loginHeading.setTypeface(varelaRound);
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+                String email = inputEmail.getText().toString().trim();
+                String password = inputPassword.getText().toString();
+
+                if(email.isEmpty()){
+                    Toast.makeText(LoginActivity.this, "Please enter email", Toast.LENGTH_LONG).show();
+                }else if(password.isEmpty()){
+                    Toast.makeText(LoginActivity.this, "Please enter password", Toast.LENGTH_LONG).show();
+                }else {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
             }
         });
 
--- app/src/main/java/com/example/joel/scavenger/ui/SignupActivity.java
+++ app/src/main/java/com/example/joel/scavenger/ui/SignupActivity.java
@@ -1,11 +1,14 @@
 package com.example.joel.scavenger.ui;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.joel.scavenger.R;
 
@@ -16,19 +19,35 @@ public class SignupActivity extends AppCompatActivity {
 
     @BindView(R.id.toLoginLink) TextView toLoginLink;
     @BindView(R.id.signUpButton) Button signUpButton;
+    @BindView(R.id.emailField) EditText inputEmail;
+    @BindView(R.id.password) EditText inputPassword;
+    @BindView(R.id.registerHeading) TextView registerHeading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+
         ButterKnife.bind(this);
+
+        Typeface varelaRound = Typeface.createFromAsset(getAssets(), "fonts/VarelaRound-Regular.ttf");
+        registerHeading.setTypeface(varelaRound);
 
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(SignupActivity.this, MainActivity.class);
-                startActivity(intent);
+                String email = inputEmail.getText().toString().trim();
+                String password = inputPassword.getText().toString();
+
+                if(email.isEmpty()){
+                    Toast.makeText(SignupActivity.this, "Please enter email", Toast.LENGTH_LONG).show();
+                }else if(password.isEmpty()){
+                    Toast.makeText(SignupActivity.this, "Please enter password", Toast.LENGTH_LONG).show();
+                }else {
+                    Intent intent = new Intent(SignupActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
             }
         });
 
