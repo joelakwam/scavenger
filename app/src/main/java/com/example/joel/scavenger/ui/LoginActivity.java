fa735abee3c43b54c0b6965084947315de0c4e3b
JoelAkwam <jakwam@gmail.com>
2018-11-21 17:44:52 +0300

    Create LoginActivity

---
+++ app/src/main/java/com/example/joel/scavenger/ui/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.joel.scavenger.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.joel.scavenger.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
