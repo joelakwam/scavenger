654e7f9e83ae54fae23ae19cd99a3eaaaebcff01
JoelAkwam <jakwam@gmail.com>
2018-11-21 17:24:28 +0300

    Create EditText field for Email in the sign up activity

--- app/src/main/java/com/example/joel/scavenger/ui/SignupActivity.java
+++ app/src/main/java/com/example/joel/scavenger/ui/SignupActivity.java
@@ -1,7 +1,9 @@
-package com.example.joel.scavenger;
+package com.example.joel.scavenger.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.joel.scavenger.R;
 
 public class SignupActivity extends AppCompatActivity {
 
