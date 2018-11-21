db67907f465132b41213541cba8f688c72c5ac7f
JoelAkwam <jakwam@gmail.com>
2018-11-21 18:28:16 +0300

    Attach clickListener to TextView in LoginActivity

--- app/src/main/java/com/example/joel/scavenger/ui/LoginActivity.java
+++ app/src/main/java/com/example/joel/scavenger/ui/LoginActivity.java
@@ -1,15 +1,32 @@
 package com.example.joel.scavenger.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.joel.scavenger.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class LoginActivity extends AppCompatActivity {
+    @BindView(R.id.toRegisterLink) TextView toRegisterLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+
+        toRegisterLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
