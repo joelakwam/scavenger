6e897c6e1c9e378d5af0c66b92577902583e2550
JoelAkwam <jakwam@gmail.com>
2018-11-22 10:22:30 +0300

    Add intent to navigate to MainActivity when user creation is successfull

--- app/src/main/java/com/example/joel/scavenger/ui/SignupActivity.java
+++ app/src/main/java/com/example/joel/scavenger/ui/SignupActivity.java
@@ -79,19 +79,19 @@ public class SignupActivity extends AppCompatActivity {
 
                     //For creating a new user
                     auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
-//                        @Override
-//                        public void onComplete(Task<AuthResult> task) {
-//                            if (!task.isSuccessful()) {
-//                                Toast.makeText(SignupActivity.this, "Authentication failed." + task.getException(), Toast.LENGTH_SHORT).show();
-//                            } else {
-//                                startActivity(new Intent(SignupActivity.this, MainActivity.class));
-//                                finish();
-//                            }
-//                        }
-//                    });
-//                }
-//            }
-//        });
+                        @Override
+                        public void onComplete(Task<AuthResult> task) {
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(SignupActivity.this, "Authentication failed." + task.getException(), Toast.LENGTH_SHORT).show();
+                            } else {
+                                startActivity(new Intent(SignupActivity.this, MainActivity.class));
+                                finish();
+                            }
+                        }
+                    });
+                }
+            }
+        });
     }
 
     @Override
