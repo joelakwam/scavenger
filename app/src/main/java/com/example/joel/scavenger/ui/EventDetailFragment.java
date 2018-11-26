98f499d20dd9e4f2b3b8cdd3455f49cc09c3cc38
JoelAkwam <jakwam@gmail.com>
2018-11-26 11:48:50 +0300

    Create EventPagerAdapter in the adapter sub-package

--- app/src/main/java/com/example/joel/scavenger/ui/EventDetailFragment.java
+++ app/src/main/java/com/example/joel/scavenger/ui/EventDetailFragment.java
@@ -2,29 +2,64 @@ package com.example.joel.scavenger.ui;
 
 
 import android.os.Bundle;
+import android.os.Parcel;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.joel.scavenger.R;
+import com.example.joel.scavenger.models.Event;
+import com.squareup.picasso.Picasso;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+import org.parceler.Parcels;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
 public class EventDetailFragment extends Fragment {
+    @BindView(R.id.eventImageView) ImageView imageLabel;
+    @BindView(R.id.eventNameTextView) TextView nameLabel;
+    @BindView(R.id.websiteUrl) TextView websiteUrl;
+    @BindView(R.id.eventDescription) TextView eventDescription;
+
+    private Event event;
+
+    public static EventDetailFragment newInstance(Event event){
+        EventDetailFragment eventDetailFragment = new EventDetailFragment();
+        Bundle args = new Bundle();
+        args.putParcelable("event", Parcels.wrap(event));
+        eventDetailFragment.setArguments(args);
+        return eventDetailFragment;
+    }
 
 
     public EventDetailFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        event = Parcels.unwrap(getArguments().getParcelable("event"));
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_event_detail, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_event_detail, container, false);
+
+        ButterKnife.bind(this, view);
+
+        Picasso.with(view.getContext()).load(event.getLogoUrl()).into(imageLabel);
+
+        nameLabel.setText(event.getName());
+        websiteUrl.setText(event.getUrl());
+        eventDescription.setText(event.getDescription());
+
+        return view;
     }
 
 }
