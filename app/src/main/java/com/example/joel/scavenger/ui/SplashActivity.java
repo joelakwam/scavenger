fc1e8ce1c47e91ec127c43ed983d364825da7d70
JoelAkwam <jakwam@gmail.com>
2018-11-26 19:05:44 +0300

    Add animation to SplashActivity

--- app/src/main/java/com/example/joel/scavenger/ui/SplashActivity.java
+++ app/src/main/java/com/example/joel/scavenger/ui/SplashActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 import com.example.joel.scavenger.R;
@@ -15,6 +17,7 @@ import butterknife.ButterKnife;
 public class SplashActivity extends Activity {
 
     @BindView(R.id.appTitle) TextView appTitle;
+    Animation upToDown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,9 @@ public class SplashActivity extends Activity {
         setContentView(R.layout.splash_file);
 
         ButterKnife.bind(this);
+
+        upToDown = AnimationUtils.loadAnimation(this, R.anim.up_to_down);
+        appTitle.setAnimation(upToDown);
 
         //Setting custom font
         Typeface pacifico = Typeface.createFromAsset(getAssets(), "fonts/Pacifico.ttf");
